d06e50f1d3bd18bdd706ddb432be030f29f99f84
Sarah Hart <sarah.hart.nc@gmail.com>
2022-03-07 16:59:09 -0500

    basic code

---
+++ develop/js/script.js
@@ -0,0 +1,8 @@
+// sets the time to the current day
+const todayDate = moment().format("dddd, MMM Do YYYY");
+
+
+//color code based on if it is in the past, present or future
+
+//save events to local storage
+
