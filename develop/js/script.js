d20e68466ab39c4d9885147ea1c612d9432387a9
Sarah Hart <sarah.hart.nc@gmail.com>
2022-03-07 17:05:01 -0500

    basic psedo code

--- develop/js/script.js
+++ develop/js/script.js
@@ -3,6 +3,14 @@ const todayDate = moment().format("dddd, MMM Do YYYY");
 
 
 //color code based on if it is in the past, present or future
+if (currentHour < 9) {
+    .addClass("future");
+} else if (currentHour > 9) {
+    .addClass("past");
+} else if (currentHour = 9) {
+    .addClass("present");
+}
+// do something like above until 6pm. 
 
 //save events to local storage
 
