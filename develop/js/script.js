ffcd1cd5981ec2258eb00c0ecd12a8645a46e769
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-02 17:23:18 -0400

    start trying code

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,15 +1,35 @@
 // sets the time to the current day
-const todayDate = moment().format("dddd, MMM Do YYYY");
+const currentDate = moment().format("dddd, MMM Do YYYY");
 
+// current time to current date
+currentTime.text(moment().format("dddd, MMMM Do"));
 
 //color code based on if it is in the past, present or future
-if (currentHour < 9) {
-    .addClass("future");
-} else if (currentHour > 9) {
-    .addClass("past");
-} else if (currentHour = 9) {
-    .addClass("present");
+// if (currentHour < 9) {
+//     .addClass("future");
+// } else if (currentHour > 9) {
+//     .addClass("past");
+// } else if (currentHour = 9) {
+//     .addClass("present");
+// }
+
+var colorCards = function (timeAndday) {
+    // gets current time
+    var currentHour = moment().format();
+    
+    //compares hours 
+    if (moment(currentHour, 'YYYY-MM-D').isBefore(currentDate)){
+        // add class for future
+        $("#input"+ index).addClass("future");
+    } else if (moment(currentHour, 'YYYY-MM-D').isAfter(currentDate)){
+        // add class for past
+        $("#input"+ index).addClass("past");
+    } else {
+        // add present class
+        $("#input"+ index).addClass("present");
+    }
 }
+
 // do something like above until 6pm. 
 
 //save events to local storage
