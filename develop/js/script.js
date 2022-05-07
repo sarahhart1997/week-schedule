84a51e00c781172dcc09db9c4be9f6f0e422587a
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-07 16:00:22 -0400

    update time in jumbotron

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,31 +1,11 @@
-// sets the time to the current day
-const currentDate = moment().format("dddd, MMM Do YYYY");
-
-// current time to current date
-currentTime.text(moment().format("dddd, MMMM Do"));
-
-// load events 
-var loadEvents = function() {
-    $("#block-8a .description").val(localStorage.getItem("block-8a"));
-    $("#block-9a .description").val(localStorage.getItem("block-9a"));
-    $("#block-10a .description").val(localStorage.getItem("block-10a"));
-    $("#block-11a .description").val(localStorage.getItem("block-11a"));
-    $("#block-12p .description").val(localStorage.getItem("block-12p"));
-    $("#block-1p .description").val(localStorage.getItem("block-1p"));
-    $("#block-2p .description").val(localStorage.getItem("block-2p"));
-    $("#block-3p .description").val(localStorage.getItem("block-3p"));
-    $("#block-4p .description").val(localStorage.getItem("block-4p"));
-    $("#block-5p .description").val(localStorage.getItem("block-5p"));
-    $("#block-6p .description").val(localStorage.getItem("block-6p"));
-    $("#block-7p .description").val(localStorage.getItem("block-7p"));
-    $("#block-8p .description").val(localStorage.getItem("block-8p"));
-}
+// current day for jumbotron
+var today = moment().format("dddd, MMM Do YYYY");
+document.getElementById("currentDay").textContent = today;
 
 // loops through time to create card 
 var mainStart = function() {
     for(let i=9; i < hours; i++) {
         colorCards(formatTime(i), i);
-        loadEvents(formatTime(i), i);
     }
 }
 
@@ -41,12 +21,13 @@ var mainStart = function() {
 var colorCards = function (dateAndtime) {
     // gets current time
     var currentHour = moment().format();
-    
+    console.log(currentHour)
+
     //compares hours 
-    if (moment(currentHour, 'YYYY-MM-D').isBefore(currentDate)){
+    if (moment(currentHour).isBefore(currentDate)){
         // add class for future
         $("#input"+ index).addClass("future");
-    } else if (moment(currentHour, 'YYYY-MM-D').isAfter(currentDate)){
+    } else if (moment(currentHour).isAfter(currentDate)){
         // add class for past
         $("#input"+ index).addClass("past");
     } else {
@@ -65,5 +46,22 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     });
 })
+
+// load events 
+var loadEvents = function() {
+    $("#block-8a .description").val(localStorage.getItem("block-8a"));
+    $("#block-9a .description").val(localStorage.getItem("block-9a"));
+    $("#block-10a .description").val(localStorage.getItem("block-10a"));
+    $("#block-11a .description").val(localStorage.getItem("block-11a"));
+    $("#block-12p .description").val(localStorage.getItem("block-12p"));
+    $("#block-1p .description").val(localStorage.getItem("block-1p"));
+    $("#block-2p .description").val(localStorage.getItem("block-2p"));
+    $("#block-3p .description").val(localStorage.getItem("block-3p"));
+    $("#block-4p .description").val(localStorage.getItem("block-4p"));
+    $("#block-5p .description").val(localStorage.getItem("block-5p"));
+    $("#block-6p .description").val(localStorage.getItem("block-6p"));
+    $("#block-7p .description").val(localStorage.getItem("block-7p"));
+    $("#block-8p .description").val(localStorage.getItem("block-8p"));
+}
 
 mainStart();
